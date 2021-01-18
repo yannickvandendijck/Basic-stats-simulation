6335ba88c934d372b53d2befc22b9c3b84acbba9
Vandendijck <YVanden2@its.jnj.com>
2021-01-18 15:49:27 +0100

    Update Power Continuous vs Dichotomization [Gaussian data].R

--- Power Continuous vs Dichotomization [Gaussian data].R
+++ Power Continuous vs Dichotomization [Gaussian data].R
@@ -50,7 +50,7 @@ for(b in 1:B){
   sim.table = t(matrix(c(n11, n12, n21, n22), nrow=2))
   
   # tests
-  result.test1 = chisq.test(sim.table, correct = FALSE)
+  result.test1 = t.test(y1, y2, var.equal = TRUE)
   result.test2 = fisher.test(sim.table)
   
   # save p-values
@@ -60,7 +60,6 @@ for(b in 1:B){
 
 mean(pvalues.test1 < 0.05)  # should be around 0.05
 mean(pvalues.test2 < 0.05)  # should be around 0.05
-mean(pvalues.test3 < 0.05)  # should be around 0.05
 
 
 ###**********************************************************************************************
@@ -71,53 +70,46 @@ mean(pvalues.test3 < 0.05)  # should be around 0.05
 
 ## Set Parameters
 set.seed(2021)
-p1 = 0.73
-p2 = 0.46       # probability of succes in group 1 and 2. Should be equal since H0 is true
-N1 = 15            # set sample size in both groups; this can be different if you want this
-N2 = 59    
-B = 10000       # number of simulations
+B = 10000         # number of simulations
+mu1 = 0.0
+delta = 0.5      # effect size
+mu2 = mu1 + delta
+sd1 = sd2 = 1.0   # sd in group 1 and 2. Equal is an assumption of Student t-test
+N1 = N2 = 100      # set sample size in both groups; this can be different if you want this
 
-## Power calculations 
-library(Exact)
-power.exact.test(p1=p1, p2=p2, n1=N1, n2=N2, method="chisq")
-power.exact.test(p1=p1, p2=p2, n1=N1, n2=N2, method="yates chisq")
-power.exact.test(p1=p1, p2=p2, n1=N1, n2=N2, method="Fisher")
+
+## Power of t-test
+power.t.test(n = 50, delta = delta, sd = sd1)
+
 
 ## Simulation
-pvalues.test1 = pvalues.test2 = pvalues.test3 = NULL
+pvalues.test1 = pvalues.test2 = NULL
 for(b in 1:B){
   # simulate group 1 data
-  n11 = rbinom(n = 1, size = N1, prob = p1) 
-  n12 = N1 - n11
+  y1 = rnorm(n = N1, mean = mu1, sd = sd1)
+  y1.dich = 1*(y1 < qnorm(p = 0.25))
+  n11 = sum(y1.dich==0)
+  n12 = sum(y1.dich==1)
   
   # simulate group 1 data
-  n21 = rbinom(n = 1, size = N2, prob = p2) 
-  n22 = N2 - n21
+  y2 = rnorm(n = N2, mean = mu2, sd = sd2)
+  y2.dich = 1*(y2 < qnorm(p = 0.25))
+  n21 = sum(y2.dich==0)
+  n22 = sum(y2.dich==1)
   
-  # simulated 2x2 table
+  # Create 2x2 table
   sim.table = t(matrix(c(n11, n12, n21, n22), nrow=2))
   
   # tests
-  result.test1 = chisq.test(sim.table, correct = FALSE)
-  result.test2 = chisq.test(sim.table, correct = TRUE)
-  result.test3 = fisher.test(sim.table)
+  result.test1 = t.test(y1, y2, var.equal = TRUE)
+  result.test2 = fisher.test(sim.table)
   
   # save p-values
   pvalues.test1[b] = result.test1$p.value
   pvalues.test2[b] = result.test2$p.value
-  pvalues.test3[b] = result.test3$p.value
 }
 
 100*mean(pvalues.test1 < 0.05)  # power of test 1
 100*mean(pvalues.test2 < 0.05)  # power of test 2
-100*mean(pvalues.test3 < 0.05)  # power of test 3
-
-
-
-
-
-
-
-
 
 
